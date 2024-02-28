67a312602d45f8f0eada0d78ad82b5def077e5c0
davi-barross <davibarrosgonzaga@gmail.com>
2024-02-28 16:27:50 -0300

    mensagem do commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let quantidadesdevitoria = 10; 
+ 
+
+if (quantidadesdevitoria <= 10) {
+  console.log("Opa opa opa, você possui a patente ferro!");
+} else if (quantidadesdevitoria <= 20) {
+  console.log("você possui a patente de Bronze");
+} else if (quantidadesdevitoria <= 50) {
+  console.log("Você possui a patente de Prata");
+} else if (quantidadesdevitoria <= 80) {
+  console.log("você possui a patente de Ouro");
+} else if (quantidadesdevitoria <= 90) {
+  console.log("você possui a patente de Diamante");
+} else if (quantidadesdevitoria <= 100) {
+  console.log("você possui a patente lendária");
+} else if (quantidadesdevitoria >= 101) {
+  console.log("você possui a patente de Imortal");
+} 
+ 
